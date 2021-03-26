876d6b3ae882b18bf4117d71dcc2411c4cbb63dc
saqla <whitecross8fortune8fatedress@gmail.com>
2021-03-26 21:23:21 +0900

    💄 styleの設定

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,8 @@
-import styles from "../styles/Home.module.css";
-
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <h1 className={styles.logo_name}>光陽</h1>
+    <header className="header">
+      <div className="logo">
+        <h1 className="logo-title">光陽</h1>
       </div>
     </header>
   );
