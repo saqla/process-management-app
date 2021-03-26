ce79ffe3df7d561eb2fa564938874e62c6172e65
saqla <whitecross8fortune8fatedress@gmail.com>
2021-03-26 23:34:21 +0900

    💄 新規作成ボタン

--- components/HouseMakers.jsx
+++ components/HouseMakers.jsx
@@ -1,11 +1,17 @@
+import Image from 'next/image'
+
 export default function HouseMaker() {
   return (
-    <h2 className="contents-title">ハウスメーカー</h2>
-
-    // <header className={styles.header}>
-    //   <div className={styles.logo}>
-    //     <h1 className={styles.logo_name}>光陽</h1>
-    //   </div>
-    // </header>
+    <div className="w-80 h-12 my-2.5 flex justify-center items-center relative">
+      <h2 className="contents-title">ハウスメーカー</h2>
+      <div className="btn">
+        <Image
+          src="/pencil-alt.svg"
+          alt="新規作成ボタン"
+          width={38}
+          height={38}
+        />
+      </div>
+    </div>
   );
 }
