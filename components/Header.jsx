54447a1f7cafa3540f136b1934572216bdfb60c5
saqla <whitecross8fortune8fatedress@gmail.com>
2021-03-30 21:03:49 +0900

    🔥 src/に移動した為

--- components/Header.jsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="header">
-      <div className="logo">
-        <h1 className="logo-title">光陽</h1>
-      </div>
-    </header>
-  );
-}
